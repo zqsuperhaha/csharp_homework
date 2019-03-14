84ca1d1588dfde4614d73fc26878989901fd17db
mypc <1459231260@qq.com>
2019-03-14 13:52:26 +0800

    补全代码

--- week03/Demo_score/Demo_score/Program.cs
+++ week03/Demo_score/Demo_score/Program.cs
@@ -9,10 +9,14 @@ namespace Demo_score
     {
         static void Main(string[] args)
         {
-            int c = 78, csharp = 90, java = 63;
+            double c = 78, csharp = 90, java = 63;
+            double avgscore = (c + csharp + java) / 3;
+            Console.WriteLine("------------------------------");
             Console.WriteLine("课程\tC\tC#\tJAVA");
             Console.WriteLine("成绩\t{0}\t{1}\t{2}",c,csharp,java);
+            Console.WriteLine("------------------------------");
             Console.WriteLine("C#与JAVA的分数差为:{0}", Math.Abs(csharp-java));
+            Console.WriteLine("三门课程的平均分为:"+avgscore+"\n");
            
         }
     }
