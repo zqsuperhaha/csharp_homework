c4cca2d6333d1e46d9d350e5c1278c379a261acd
Echowll <123>
2019-03-23 18:58:09 +0800

    add week04 all

---
+++ week04/Colorful_String/Colorful_String/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colorful_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 字符串 操作
+            string str = "我喜欢吃冰淇淋";
+            Console.WriteLine(str);
+            // 1.增
+            string s1 = str.Insert(4,"超级大的");
+            Console.WriteLine(s1);
+
+        }
+    }
+}
